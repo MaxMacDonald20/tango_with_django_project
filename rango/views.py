718794a0666655c7fb9c330e796fa4f8eaf6d03e
Max MacDonald <2567568m@student.gla.ac.uk>
2022-01-29 00:26:29 +0000

    chapter 5, up to exercises

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Curnchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context = context_dict)
 def about(request):
     return render(request, 'rango/about.html')
